a1c5b53e9352b116373ada08388bf63d0f5884e0
JP-Norton <72099534+JP-Norton@users.noreply.github.com>
2024-12-05 13:35:33 -0500

    working version not done

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,9 +1,19 @@
+import mongoose from "mongoose";
 import * as modulesDao from "./dao.js";
+
 export default function ModuleRoutes(app) {
   app.delete("/api/modules/:moduleId", async (req, res) => {
-    const { moduleId } = req.params;
-    const status = await modulesDao.deleteModule(moduleId);
-    res.send(status);
+    try {
+      const { moduleId } = req.params;
+      // Add validation
+      if (!mongoose.Types.ObjectId.isValid(moduleId)) {
+        return res.status(400).json({ error: "Invalid module ID format" });
+      }
+      const status = await modulesDao.deleteModule(moduleId);
+      res.send(status);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   });
   app.put("/api/modules/:moduleId", async (req, res) => {
     const { moduleId } = req.params;
--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -3,10 +3,10 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function UserRoutes(app) {
-  const createCourse = (req, res) => {
+  const createCourse = async (req, res) => {
     const currentUser = req.session["currentUser"];
-    const newCourse = courseDao.createCourse(req.body);
-    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+    const newCourse = await courseDao.createCourse(req.body);
+    await enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
     res.json(newCourse);
   };
   app.post("/api/users/current/courses", createCourse);
